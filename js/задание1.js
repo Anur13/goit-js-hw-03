d7b833b6d489d440dd6e6a9438bec8f4ed4eaa06
Anur13 <akrupskyi13@gmail.com>
2020-12-07 12:06:58 +0200

    1

--- js/задание1.js
+++ js/задание1.js
@@ -1,18 +1,17 @@
 const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
+    name: "Mango",
+    age: 20,
+    hobby: "html",
+    premium: true,
+}
 
-};
-
-user.mood = 'happy'
-user.hobby = 'skydiving'
+user.mood = "happy"
+user.hobby = "skydiving"
 user.premium = false
 const keys = Object.keys(user)
 // console.log(keys);
 for (const key of keys) {
-  console.log(key,`:`,user[key]);
+    console.log(key, `:`, user[key])
 }
 
 // for (const key in user) {
--- js/задание7.js
+++ js/задание7.js
@@ -3,8 +3,8 @@
  * Можно положить либо снять деньги со счета.
  */
 const Transaction = {
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw",
+    DEPOSIT: "deposit",
+    WITHDRAW: "withdraw",
 }
 
 /*
@@ -12,50 +12,93 @@ const Transaction = {
  */
 
 const account = {
-  // Текущий баланс счета
-  balance: 0,
+    // Текущий баланс счета
+    balance: 0,
 
-  // История транзакций
-  transactions: [],
+    // История транзакций
+    transactions: [],
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {},
+    /*
+     * Метод создает и возвращает объект транзакции.
+     * Принимает сумму и тип транзакции.
+     */
+    createTransaction(amount, type) {
+        let transaction = {
+            type,
+            amount,
+            id: this.transactions.length,
+        }
+        return transaction
+    },
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {},
+    /*
+     * Метод отвечающий за добавление суммы к балансу.
+     * Принимает сумму транзакции.
+     * Вызывает createTransaction для создания объекта транзакции
+     * после чего добавляет его в историю транзакций
+     */
+    deposit(amount) {
+        this.createTransaction(amount, `deposit`), (this.balance += amount)
+        this.transactions.push(this.createTransaction(amount, `deposit`))
+    },
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {},
+    /*
+     * Метод отвечающий за снятие суммы с баланса.
+     * Принимает сумму танзакции.
+     * Вызывает createTransaction для создания объекта транзакции
+     * после чего добавляет его в историю транзакций.
+     *
+     * Если amount больше чем текущий баланс, выводи сообщение
+     * о том, что снятие такой суммы не возможно, недостаточно средств.
+     */
+    withdraw(amount) {
+        this.createTransaction(amount, `withdraw`), this.transactions.push(this.createTransaction(amount, `withdraw`))
+        if (amount > this.balance) {
+            console.log(`снятие такой суммы не возможно, недостаточно средств.`)
+        }
+        this.balance -= amount
+    },
 
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {},
+    /*
+     * Метод возвращает текущий баланс
+     */
+    getBalance() {
+        return this.balance
+    },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {},
+    /*
+     * Метод ищет и возвращает объект транзации по id
+     */
+    getTransactionDetails(id) {
+        return account.transactions[id]
+    },
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  getTransactionTotal(type) {},
+    /*
+     * Метод возвращает количество средств
+     * определенного типа транзакции из всей истории транзакций
+     */
+    getTransactionTotal(type) {
+        let total = 0
+
+        for (let key of this.transactions) {
+            // console.log(key.amount)
+            if (key.type === type) {
+                total += key.amount
+            }
+        }
+        console.log(total)
+    },
 }
+console.log(account.deposit(500))
+console.log(account.deposit(1500))
+
+console.log(account.deposit(500))
+console.log(account.withdraw(400))
+console.log(account.withdraw(400))
+
+// console.log(account.balance)
+// console.log(account.transactions)
+console.log(account.getBalance())
+// console.log(account.getTransactionDetails(0))
+// console.log(account.transactions[0])
+console.log(account.getTransactionTotal(`deposit`))
