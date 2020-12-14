ce2b0c11330aff3536118627c5cae1563480b84b
Anur13 <akrupskyi13@gmail.com>
2020-12-14 10:18:40 +0200

    Update task5.js

--- js/task5.js
+++ js/task5.js
@@ -4,6 +4,7 @@ const products = [
     { name: "Дроид", price: 400, quantity: 7 },
     { name: "Захват", price: 1200, quantity: 2 },
 ]
+// вариант 1
 
 // const getAllPropValues = function (arr, prop) {
 //     let category = []
@@ -26,18 +27,38 @@ const products = [
 
 // console.log(getAllPropValues(products, "price")) // []
 
+// вариант 2
+
+// const getAllPropValues = function (arr, prop) {
+//     let category = []
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let values = Object.entries(arr[i])
+//         // console.log(values)
+//         for (let j = 0; j < values.length; j++) {
+//             let value = values[j]
+//             // console.log(value)
+//             if (value[0] === prop) {
+//                 category.push(value[1])
+//             }
+//         }
+//     }
+//     return category
+// }
+
+// console.log(getAllPropValues(products, "name")) // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, "quantity")) // [4, 3, 7, 2]
+
+// console.log(getAllPropValues(products, "price")) // []
+
+// вариант 3
+
 const getAllPropValues = function (arr, prop) {
     let category = []
-
-    for (let i = 0; i < arr.length; i++) {
-        let values = Object.entries(arr[i])
-        // console.log(values)
-        for (let j = 0; j < values.length; j++) {
-            let value = values[j]
-            // console.log(value)
-            if (value[0] === prop) {
-                category.push(value[1])
-            }
+    for (let product of arr) {
+        if (Object.keys(product).includes(prop)) {
+            category.push(product[prop])
         }
     }
     return category
